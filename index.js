395e0af5fbd36671e93066fa71af126fddead43a
freenih10 <freenih10@gmail.com>
2024-06-13 10:50:21 +0700

    $

--- index.js
+++ index.js
@@ -1,101 +1,92 @@
 const { Telegraf } = require("telegraf");
 const { message } = require("telegraf/filters");
 const axios = require("axios");
+const FormData = require("form-data");
 require("dotenv").config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
-bot.start((ctx) =>
-  ctx.reply(`
+bot.start(ctx =>
+    ctx.reply(`
 List Command:
 
-/apkru + angka 1-10 - Menampilkan aplikasi mod dari Androeed.ru (example: /apkru 5)
+/hdimg - Create your image hd
+/removebg - Remove background image
 `)
 );
 
-bot.command("apkru", async (ctx) => {
-  const input = ctx.message.text.split(" ");
-  const count = input[1] ? parseInt(input[1]) : 10;
+//  const apiResponse = await axios.get(
+//                 `https://api.nyxs.pw/tools/hd?url=${localFileUrl}`
+//             );
 
-  const maxResults = Math.min(count, 10);
-
-  try {
-    const response = await axios.get("https://apkru.vercel.app/apkru");
-    const result = response.data;
-
-    for (let i = 0; i < maxResults; i++) {
-      const item = result[i];
-      if (!item) break;
-
-      let message = `
-Nama: 
-<code>${item.title}</code>
-
-Size: 
-<code>${item.size}</code>
-
-Date: 
-<code>${item.date}</code>
-
-Version: 
-<code>${item.version}</code>
-
-Link: 
-${item.url}
-
-Download:
-`;
-      item.download.forEach((download) => {
-        message += `<a href="${download.url}">${download.title}</a>`;
-      });
-
-      await ctx.telegram.sendChatAction(ctx.chat.id, "upload_photo");
-      await ctx.replyWithPhoto(item.img, {
-        caption: message,
-        parse_mode: "HTML",
-      });
-    }
-  } catch (error) {
-    console.error(error);
-    ctx.reply("Terjadi kesalahan saat mengambil data.");
-  }
-});
 const commandResponses = {
-  "/hdimage": {
-    response: "Ini adalah pesan respons untuk perintah /hdimage.",
-    handler: async (ctx) => {
-      // Logika khusus untuk /hdimage di sini
-      await ctx.reply("Mengirimkan gambar definisi tinggi...");
+    "/hdimg": {
+        response: "Image HD sedang di proses...",
+        handler: async ctx => {
+            try {
+                const photoArray = ctx.message.photo;
+                const fileId = photoArray[photoArray.length - 1].file_id;
+                const file = await ctx.telegram.getFile(fileId);
+                const fileUrl = `https://api.telegram.org/file/bot${process.env.BOT_TOKEN}/${file.file_path}`;
+
+                const formData = new FormData();
+                formData.append("image", fileUrl);
+
+                const imgbbResponse = await axios.post(
+                    `https://api.imgbb.com/1/upload?expiration=1800&key=${process.env.API_IMGBB}`,
+                    formData,
+                    {
+                        headers: { ...formData.getHeaders() }
+                    }
+                );
+
+                const imgbbData = imgbbResponse.data.data;
+
+                const hdResponse = await axios.get(
+                    `https://api.nyxs.pw/tools/hd?url=${imgbbData.url}`
+                );
+
+                await ctx.sendPhoto(
+                    ctx.chat.id,
+                    `https://api.nyxs.pw/tools/hd?url=${imgbbData.url}`
+                );
+
+              await  ctx.reply("Image HD berhasil dikirim.");
+            } catch (error) {
+                console.error("Error:", error.message);
+                ctx.reply("Terjadi kesalahan saat memproses gambar.");
+            }
+        }
     },
-  },
-  "/removebg": {
-    response: "Ini adalah pesan respons untuk perintah /removebg.",
-    handler: async (ctx) => {
-      // Logika khusus untuk /removebg di sini
-      await ctx.reply("Menghapus latar belakang gambar...");
-    },
-  },
-  // Tambahkan perintah lainnya dengan respons masing-masing di sini
-};
-bot.on("photo", async (ctx) => {
-  const caption = ctx.message.caption ? ctx.message.caption.toLowerCase() : "";
-  const command = commandResponses[caption];
-  if (command) {
-    const message = await ctx.reply(command.response);
-    if (command.handler) {
-      await command.handler(ctx);
-      await ctx.telegram.deleteMessage(ctx.chat.id, message.message_id);
+
+    "/removebg": {
+        response: "Remove background sedanf di proses...",
+        handler: async ctx => {
+            // Logika khusus untuk /removebg di sini
+            await ctx.reply("Menghapus latar belakang gambar...");
+        }
     }
-    // Menghapus pesan respons setelah penanganan selesai
-  } else {
-    await ctx.reply("Perintah tidak dikenali.");
-  }
+};
+bot.on("photo", async ctx => {
+    const caption = ctx.message.caption
+        ? ctx.message.caption.toLowerCase()
+        : "";
+    const command = commandResponses[caption];
+    if (command) {
+        const message = await ctx.reply(command.response);
+        if (command.handler) {
+            await command.handler(ctx);
+            await ctx.telegram.deleteMessage(ctx.chat.id, message.message_id);
+        }
+    } else {
+        await ctx.reply("Perintah tidak dikenali.");
+    }
 });
 
 bot.launch();
 
 module.exports = (req, res) => {
-  bot.handleUpdate(req.body, res);
-  return res.status(200).end();
+    bot.handleUpdate(req.body, res);
+    return res.status(200).end();
 };
 
 process.once("SIGINT", () => bot.stop("SIGINT"));
